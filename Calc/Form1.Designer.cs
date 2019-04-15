9f4607f8eb6b2d9a3c198cace725087fa78ae63b
Kalungi <charles.kalungi09@gmail.com>
2019-04-15 21:09:05 +0800

    Add files via upload

---
+++ Calc/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtNumbers = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(193, 48);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(77, 23);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.BtnGenerate_Click);
+            // 
+            // txtNumbers
+            // 
+            this.txtNumbers.Location = new System.Drawing.Point(49, 80);
+            this.txtNumbers.Multiline = true;
+            this.txtNumbers.Name = "txtNumbers";
+            this.txtNumbers.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtNumbers.Size = new System.Drawing.Size(221, 237);
+            this.txtNumbers.TabIndex = 1;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(49, 323);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(57, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter the number of Problems to be generated";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(193, 324);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Export";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 369);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtNumbers);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtNumbers;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Calc/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Calc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGenerate_Click(object sender, EventArgs e)
+        {
+            int parsedValue;
+            if (!int.TryParse(textBox1.Text, out parsedValue))
+            {
+                MessageBox.Show("Enter Number of problems to generate");
+            }
+            else { 
+            
+            Random rnd1 = new Random();
+            Random rnd2 = new Random();
+            Random rnd3 = new Random();
+            Random operatorSelect = new Random();
+
+            int nu1 = rnd1.Next(0, 20);
+            int nu2 = rnd2.Next(0, 50);
+            int nu3 = rnd3.Next(0, 101);
+
+            int mathOperator = 0;
+            string newOperator = "";
+            int mathOperator2 = 0;
+            string newOperator2 = "";
+
+            mathOperator = operatorSelect.Next(5);
+            switch (mathOperator)
+            {
+                case 1:
+                    newOperator = "+";
+
+                    break;
+                case 2:
+                    newOperator = "-";
+                    break;
+                case 3:
+                    newOperator = "*";
+                    break;
+                case 4:
+                    newOperator = "/";
+                    break;
+                case 5:
+                    newOperator = "+";
+                    break;
+            }
+            mathOperator2 = operatorSelect.Next(5);
+            switch (mathOperator2)
+            {
+                case 1:
+                    newOperator2 = "+";
+                    break;
+                case 2:
+                    newOperator2 = "-";
+                    break;
+                case 3:
+                    newOperator2 = "*";
+                    break;
+                case 4:
+                    newOperator2 = "/";
+                    break;
+                case 5:
+                    newOperator2 = "+";
+                    break;
+            }
+                        
+            string newLine = Environment.NewLine;
+            string ans = "";
+            double result = 0;
+
+            ans = nu1 + Convert.ToString(newOperator) + nu2 + Convert.ToString(newOperator2) + nu3;
+            result = Convert.ToDouble(new DataTable().Compute(ans, null));
+
+            if (result >= 0 && result <= 100)
+            {
+                txtNumbers.Text = nu1 + " " + newOperator + " " + nu2 + " " + newOperator2 + " " + nu3 + " = " + " " + Convert.ToString(result);
+            }
+            else {
+            }
+
+            int num1 = Convert.ToInt32(textBox1.Text);
+            for (int i = 1; i < num1; i++)
+            {
+                nu1 = rnd1.Next(0, 20);
+                nu2 = rnd2.Next(0, 50);
+                nu3 = rnd3.Next(0, 101);
+                mathOperator = operatorSelect.Next(5);
+                switch (mathOperator)
+                {
+                    case 1:
+                        newOperator = "+";
+
+                        break;
+                    case 2:
+                        newOperator = "-";
+                        break;
+                    case 3:
+                        newOperator = "*";
+                        break;
+                    case 4:
+                        newOperator = "/";
+                        break;
+                    case 5:
+                        newOperator = "+";
+                        break;
+                }
+                mathOperator2 = operatorSelect.Next(5);
+                switch (mathOperator2)
+                {
+                    case 1:
+                        newOperator2 = "+";
+                        break;
+                    case 2:
+                        newOperator2 = "-";
+                        break;
+                    case 3:
+                        newOperator2 = "*";
+                        break;
+                    case 4:
+                        newOperator2 = "/";
+                        break;
+                    case 5:
+                        newOperator2 = "+";
+                        break;
+                }
+                ans = nu1 + Convert.ToString(newOperator) + nu2 + Convert.ToString(newOperator2) + nu3;
+                result = Convert.ToDouble(new DataTable().Compute(ans, null));
+                if (result >= 0 && result <= 100)
+                {
+                    txtNumbers.Text = txtNumbers.Text + newLine + nu1 + " " + newOperator + " " + nu2 + " " + newOperator2 + " " + nu3 + " = " + " " + Convert.ToString(result);
+                }
+                else
+                {
+                }
+            }
+            textBox1.Clear();
+            }
+        }
+         private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            txtNumbers.Clear();
+            textBox1.Clear();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            TextWriter writer = new StreamWriter(@"C:\Users\betty\Desktop\Result.txt");
+            writer.Write(txtNumbers.Text);
+            writer.Close();
+
+            MessageBox.Show("Successfully Exported");
+        }
+    }
+}
